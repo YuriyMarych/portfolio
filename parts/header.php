c15e1d14c91ed20e8ea4d6373986cf5528859ddc
Yuriy Marych <stydentuss@gmail.com>
2019-03-01 12:43:20 +0200

    Created header

---
+++ parts/header.php
@@ -0,0 +1,35 @@
+<div class="top">
+    <div class="logo">
+        <img src="/assets/images/logo.jpg" alt="logo" class="logoImg">
+        <p class="siteName">Marych Yuriy</p>
+    </div>
+    <nav class="menu">
+        <ul>
+            <li><a class="menu-item" href="javascript:void(0)">work</a></li>
+            <li><a class="menu-item" href="javascript:void(0)">about me</a></li>
+            <li><a class="menu-item" href="javascript:void(0)">blog</a></li>
+            <li><a class="menu-item" href="javascript:void(0)">contact</a></li>
+            <li><a class="btn-hire" href="javascript:void(0)">hire me</a></li>
+        </ul>
+    </nav>
+</div>
+<div class="profile">
+    <div class="info">
+        <h1 class="name"><span>Hello, I`m </span><br/>Marych Yuriy</h1>
+        <p class="work">PHP Developer</p>
+        <div class="social">
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-twitter-square"></i>
+        </div>
+        <div class="buttons">
+            <a href="javascript:void(0);" class="btn-hire-black">hire me</a>
+            <a href="javascript:void(0);" class="btn-hire">see my resume</a>
+        </div>
+    </div>
+
+    <div class="photo">
+        <img src="/assets/images/photo.png" alt="photo Yuriy`s">
+    </div>
+</div>
